6891d15cef871647914211f1cc5948dc487f7744
Edvin Agnas <edvin.agnas@gmail.com>
2022-01-21 20:14:09 +0100

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,2 +1,15 @@
-// ip = "http://127.0.0.1:5000"
-ip = "http://192.168.86.50:5000"
+var vars = {};
+var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    vars[key] = value;
+});
+
+ip = "http://192.168.8.112"
+
+if (vars['ip'] == "localhost") {
+    ip = "http://127.0.0.1:5000"
+}
+else if (vars['ip'] != undefined) {
+    ip = "http://" + vars['ip']
+}
+
+console.log(ip)
