3ff06764040fe3621ff42c32df1d5471b257e7fc
Edvin Agnas <39798202+edvinag@users.noreply.github.com>
2024-01-28 19:11:01 +0000

    Change ip to Lindome

--- ip.js
+++ ip.js
@@ -3,7 +3,8 @@ var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m,
     vars[key] = value;
 });
 
-ip = "http://192.168.32.145"
+//ip = "http://192.168.32.145"
+ip = "http://192.168.1.112"
 
 // if (vars['ip'] == "localhost") {
 //     ip = "http://127.0.0.1:5000"
