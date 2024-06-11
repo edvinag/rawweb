7e272347a086a8e55ffb97911e04dc9dd33168eb
Agnas Edvin <edvin.agnas@volvo.com>
2024-06-11 17:04:28 +0200

    Add refresh of map and findIP

--- ip.js
+++ ip.js
@@ -3,18 +3,49 @@ var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m,
     vars[key] = value;
 });
 
-// Boat
-//ip = "http://192.168.32.12"
-//ip = "http://192.168.8.112"
 
-// Lars
-ip = "http://192.168.1.112"
+function findResponsiveIP() {
+    const ipList = [
+        '192.168.86.112',
+        '192.168.0.2',
+        "127.0.0.1:5000",
+        '192.168.0.4',
+        '192.168.123.19',
+        // Add more IPs as needed
+    ];
 
-// if (vars['ip'] == "localhost") {
-//     ip = "http://127.0.0.1:5000"
-// }
-// if (vars['ip'] == "Connect") {
-//     ip = "http://192.168.86.50:5000"
-// }
+    return new Promise((resolve) => {
+        let found = false;
 
-console.log(ip)
+        ipList.forEach(_ip => {
+            if (found) return;
+            console.log(`Trying IP: ${_ip}`);
+
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        if (!found) {
+                            found = true;
+                            console.log(`Found responsive IP: ${_ip}`);
+                            ip = `http://${_ip}`
+                            resolve(_ip);
+                        }
+                    }
+                }
+            };
+            xhttp.onerror = function () {
+                // Do nothing, the IP did not respond
+            };
+            xhttp.open("GET", `http://${_ip}/all`, true);
+            xhttp.send();
+        });
+
+        // If no IP responds within 5 seconds, resolve with null
+        setTimeout(() => {
+            if (!found) {
+                resolve(null);
+            }
+        }, 5000);
+    });
+}
